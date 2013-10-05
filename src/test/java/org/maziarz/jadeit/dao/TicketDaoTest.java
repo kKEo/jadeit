260d5e82fb41922b61f3ee5f400f39dde01237e9
krma <krma@maziarz.org>
2013-10-05 09:58:52 +0200

    Merge from stash

--- src/test/java/org/maziarz/jadeit/dao/TicketDaoTest.java
+++ src/test/java/org/maziarz/jadeit/dao/TicketDaoTest.java
@@ -12,29 +12,53 @@ import org.maziarz.jadeit.model.Ticket.Priority;
 public class TicketDaoTest extends BaseDaoTest {
 
 	@Resource
+<<<<<<< Updated upstream
 	TicketDao dao;
 
 	@Resource
 	RequirementDao rdao;
+=======
+	TicketDao ticketDao;
+
+	@Resource
+	RequirementDao resourceDao;
+>>>>>>> Stashed changes
 
 	@Test
 	public void testTicketDao() {
 		Ticket t = new Ticket("title1", "desc", Priority.HIGH);
-		dao.save(t);
+		ticketDao.save(t);
 		long id = t.getId();
 
+<<<<<<< Updated upstream
+
 		Ticket t2 = dao.load(id);
+=======
+		Ticket t2 = ticketDao.load(id);
+>>>>>>> Stashed changes
 
 		Assert.assertEquals("title1", t2.getName());
 
 		Requirement r = new Requirement();
 		r.setName("Req1");
 
+<<<<<<< Updated upstream
 		rdao.save(r);
 
 		r.setDescrition("Desc");
 
 		rdao.save(r);
+		resourceDao.save(r);
+
+		r.setDescrition("Desc");
+
+=======
+		resourceDao.save(r);
+
+		r.setDescrition("Desc");
+
+>>>>>>> Stashed changes
+		resourceDao.save(r);
 
 		System.out.println(r.getId());
 
